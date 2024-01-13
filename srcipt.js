6644fa1e488d4aa30b50e56e77fc2fc6640eab01
James Loesch <155027840+jamesloesch1@users.noreply.github.com>
2024-01-13 14:03:22 -0500

    Got a working script

--- srcipt.js
+++ srcipt.js
@@ -1,91 +1,75 @@
-// Computer vs Human player 
-// The computer needs to randomly choose rock, paper, or scissors 
-// The program needs to get the human players input
-// The program needs to evaluate the human player's input against the computer's 
-// Based on defined rules, the program needs to evaluate who wins the game 
 
-// Rules:
-// Rock beats scissors
-// Scissors beats paper
-// Paper beats rock 
-
-// Three variables for button selectors 
+let outcomeText = document.querySelector('.outcome.text'); 
 
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
 
-let choiceContainer = document.querySelector('.container.choice');
-
-// Initialize user choice variable to store user choice 
-
 let userChoice;
 
-// Event listeners for user choice and associated buttons 
+let userChoiceText = document.querySelector('#user-choice-text');
 
-let humanChoice = document.querySelector('#humanChoice');
+let computerChoiceContainer = document.querySelector('.computer.choice.container');
+let computerChoiceText = document.querySelector('#computer-choice-text');
 
+// Computer choice
+
+const CHOICES = ["rock", "paper", "scissors"];
+
+const computerChoice = function getComputerChoice() {
+
+    const randomIndex = Math.floor(Math.random() * 3);
+    return CHOICES[randomIndex];
+};
+
+// Player choice
 
 rock.addEventListener('click', () => {
     userChoice = 'rock';
-    humanChoice.textContent = userChoice;;
 });
 
 paper.addEventListener('click', () => {
     userChoice = 'paper';
-    humanChoice.textContent = userChoice;;
 });
 
 scissors.addEventListener('click', () => {
     userChoice = 'scissors';
-    humanChoice.textContent = userChoice;;
+});
+
+// Function to play a round and determine a winner
+
+function determineWinner(computerChoice, userChoice) {
+
+    let computer = computerChoice();
+    let human = userChoice;
+
+    userChoiceText.textContent = human;
+    computerChoiceText.textContent = computer;
+
+    if (computer === human) {
+         outcomeText.textContent = "Draw";
+    } else if (computer == "rock" && human == "scissors" || computer == "paper" && human == "rock" || computer == "scissors" && human == "paper") {
+        outcomeText.textContent = "Computer wins"
+    } else {
+        outcomeText.textContent = "Human wins";
+    }
+};
+
+rock.addEventListener('click', () => {
+    determineWinner(computerChoice, userChoice);
+});
+
+paper.addEventListener('click', () => {
+    determineWinner(computerChoice, userChoice);
+});
+
+scissors.addEventListener('click', () => {
+    determineWinner(computerChoice, userChoice);
 });
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// Possible computer choices 
-
-const CHOICES = ["rock", "paper", "scissors"];
-
-// Function to get computer choice 
-
-const computerChoice = function getComputerChoice() {
-    const randomIndex = Math.floor(Math.random() * 3);
-    return CHOICES[randomIndex];
-}
-
-// initialing the score element so we have a place to display the score 
-let score = document.querySelector('#container');
-
-
-// function determineWinner(computerChoice, userChoice) {
-
-//     let computer = computerChoice();
-//     let human = userChoice;
-
-//     console.log(computer);
-//     console.log(human);
-
-//     if (computer === human) {
-//          console.log("Draw");
-//     } else if (computer == "rock" && human == "scissors" || computer == "paper" && human == "rock" || computer == "scissors" && human == "paper") {
-//         console.log("Computer wins");
-//     } else {
-//         console.log("Human wins");
-//     }
-// }
 
 
 
